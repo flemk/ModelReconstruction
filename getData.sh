ff1b4188e2ecc704f54551768e4be88f834031c1
Andreas Greiner <andreas.greiner@uni-freiburg.de>
2020-03-18 17:46:53 +0100

    first commit

---
+++ getData.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv
+wget https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv
+wget https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv
